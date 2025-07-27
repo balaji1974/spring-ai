c8e303b1a5eb9039e97856fc9793ab3002b05b87
Balaji Thiagarajan <thiagarajan.balaji@gmail.com>
2025-07-27 22:12:48 +0300

    Prompt Assistant

---
+++ ai-prompt-assistant/src/main/java/com/bala/springboot/ai/ai_prompt_assistant/util/MathReasoning.java
@@ -0,0 +1,21 @@
+package com.bala.springboot.ai.ai_prompt_assistant.util;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public record MathReasoning(
+		  @JsonProperty(required = true, value = "steps") Steps steps,
+		  @JsonProperty(required = true, value = "final_answer") String finalAnswer
+		  ) 
+{
+		
+		record Steps(@JsonProperty(required = true, value = "items") Items[] items) {
+			record Items(
+					@JsonProperty(required = true, value = "explanation") String explanation,
+					@JsonProperty(required = true, value = "output") String output
+			) {
+				
+			}
+		}
+		
+}
